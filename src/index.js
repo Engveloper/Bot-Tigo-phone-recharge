77f38f0e0d57034600ef37a5da5c36571518906e
Engels Baca Sevilla <engelsbaca@spartansv.net>
2021-10-02 15:09:26 -0600

    Requests steps logic

--- src/index.js
+++ src/index.js
@@ -1,33 +1,80 @@
-import { tigoPing, refreshToken as refreshTokenRequest } from "./requests";
-import { getTigoTokens, updateAccessToken } from "./utils";
+import {
+  tigoPing,
+  refreshToken as refreshTokenRequest,
+  placeOrder,
+  purchaseOrder,
+  statusOrder,
+} from './requests'
+import { getTigoTokens, updateAccessToken } from './utils'
 
 export default async function main() {
+  let { lastAccessToken: accessToken, refreshToken } = getTigoTokens()
+
   try {
-    const res = await tigoPing();
-    console.log({ res });
+    const res = await tigoPing({ accessToken })
+    console.log({ tigoPing: res.body })
   } catch (error) {
-    console.log({ error });
-    const { lastAccessToken, refreshToken } = getTigoTokens();
-    if (!lastAccessToken || !refreshToken) {
+    console.log({
+      response: JSON.stringify(error.response.body, null, 2),
+    })
+
+    if (!accessToken || !refreshToken) {
       console.error(
-        "Error: You have to save your initial accessToken and refreshToken in storage.json"
-      );
-      return Promise.resolve();
+        'Error: You have to save your initial accessToken and refreshToken in storage.json'
+      )
+      return Promise.resolve()
     }
 
     const refreshTokenResponse = await refreshTokenRequest({
       refreshToken,
-      accessToken: lastAccessToken,
-    });
+      accessToken,
+    })
 
-    console.log({ refreshTokenResponse: refreshTokenResponse.body });
+    console.log({ refreshTokenResponse })
 
     if (refreshTokenResponse.statusCode === 200) {
       const {
         body: { body },
-      } = refreshTokenResponse;
+      } = refreshTokenResponse
 
-      updateAccessToken({ accessToken: body.IdToken });
+      accessToken = body.IdToken
+      updateAccessToken({ accessToken })
+    } else {
+      console.error('Error: There is an unknown error')
+      return Promise.resolve()
     }
+  }
+
+  return rechargeMobile({ accessToken })
+}
+
+async function rechargeMobile({ accessToken }) {
+  const number = `50588995215`
+  const rechargeResponse = await placeOrder({
+    accessToken,
+    amount: '10',
+    number,
+  })
+
+  console.log({ rechargeResponse, rechargeBody: rechargeResponse.body })
+  const orderId = rechargeResponse.body.config.params.purchaseorderId
+
+  if (orderId) {
+    const purchaseOrderResponse = await purchaseOrder({
+      accessToken,
+      orderId,
+      number,
+    })
+    console.log({ purchaseOrderResponse: purchaseOrderResponse.body })
+
+    const statusOrderResponse = await statusOrder({
+      accessToken,
+      orderId,
+      number,
+    })
+    console.log({ statusOrderResponse: statusOrderResponse.body })
+  } else {
+    console.error('Error: There is an unknown error')
+    return Promise.resolve()
   }
 }
